bf8b0cec22b243544e053da24d9f392e3a884898
Thomas Hobohm <superman3275@gmail.com>
2015-08-06 01:05:16 -0500

    Programmed colorscheme.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,24 +4,18 @@ class QuestionsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    question = current_user.questions.find_by(ready: true)
-    redirect_to question_path(question.id) if question
-
+    @current_question = current_user.questions.find_by(ready: true)
     @questions = Asker.where(ready: true, claimed: false).order(updated_at: :desc).limit(10)
   end
   def show
-    current_question = current_user.questions.find_by(ready: true)
-    if current_question
-      redirect_to write_question_path(current_question.id)
-    else
-      @question = Asker.find(params[:id])
-      redirect_to questions_path unless @question
-    end
+    @current_question = current_user.questions.find_by(ready: true)
+    @question = Asker.find(params[:id])
+    redirect_to questions_path unless @question
   end
   def claim
-    current_question = current_user.questions.find_by(ready: true)
-    if current_question
-      redirect_to write_question_path(current_question.id)
+    @current_question = current_user.questions.find_by(ready: true)
+    if @current_question
+      redirect_to write_question_path(@current_question.id)
     else
       @question = Asker.find(params[:id])
       redirect_to questions_path if @question && @question.claimed
@@ -43,8 +37,8 @@ class QuestionsController < ApplicationController
     redirect_to questions_path
   end
   def write
-    @question = Asker.find(params[:id])
-    redirect_to questions_path unless @question && @question == current_user.questions.find_by(ready: true)
+    @current_question = Asker.find(params[:id])
+    redirect_to questions_path unless @current_question && @current_question == current_user.questions.find_by(ready: true)
   end
   def answer
     @question = Asker.find(params[:id])
