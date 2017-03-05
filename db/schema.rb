84d8d9ff594d5530e00910af14d020273e002ac4
Thomas Hobohm <superman3275@gmail.com>
2017-03-05 01:45:58 -0600

    Redoing ruby

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -26,9 +25,8 @@ ActiveRecord::Schema.define(version: 20150806061150) do
     t.datetime "updated_at",                 null: false
     t.integer  "user_id"
     t.string   "answer"
+    t.index ["user_id"], name: "index_askers_on_user_id", using: :btree
   end
-
-  add_index "askers", ["user_id"], name: "index_askers_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -45,10 +43,9 @@ ActiveRecord::Schema.define(version: 20150806061150) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "askers", "users"
 end
